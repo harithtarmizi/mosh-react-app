833d9f3d9030c8ec8836369dac277b87650f8df1
Rith <harith.trmz@gmail.com>
2023-04-27 23:39:11 +0800

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,18 @@
 function ListGroup() {
-  // in JSX, we don't have for loop
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
 
+  // in JSX, we don't have for loop
+  // remember, inside JSX, we can't use if statement, unless have curly brackets
+
+  // how to avoid null >>> {items.length === 0 ? <p>No item found</p> : null}
+  // ex: {items.length === 0 && <p>No item found</p>}
+  // if result is true && 'Mosh', i will return 'Mosh'
+  // if result is false && 'Mosh', i will return false
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item} className="list-group-item">
