9e141ceea4c5442ac159669e39f28bdc5dd1f464
Rith <harith.trmz@gmail.com>
2023-04-27 18:42:30 +0800

    create react component

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,4 +1,9 @@
 // Pascal casing
 function Message() {
-
+  // JSX: JavaScript XML
+  const name = "Taehun";
+  if (name) return <h1>Hellow world, {name}!</h1>;
+  return <h1>Hello world!</h1>;
 }
+
+export default Message;
