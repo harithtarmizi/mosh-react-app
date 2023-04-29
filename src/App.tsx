0ed335185fc09f6d83ab932e3ae92af8e0df2f99
Rith <harith.trmz@gmail.com>
2023-04-29 15:18:10 +0800

    showing an alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisible] = useState(false);
+
   return (
     <div>
-      <Button color="secondary" onClick={() => console.log("Clicked!")}>My Button</Button>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>
+      )}
+      <Button color="secondary" onClick={() => setAlertVisible(true)}>
+        My Button
+      </Button>
     </div>
   );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -9,10 +9,22 @@ import { ReactNode } from "react";
 // chldren props mostly supported  to do it like this
 interface Props {
   children: ReactNode;
+  onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
-  return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, onClose }: Props) => {
+  return (
+    <div className="alert alert-primary alert-dismissible">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
+    </div>
+  );
 };
 
 export default Alert;
