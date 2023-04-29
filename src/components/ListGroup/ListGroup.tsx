daf2a7ea7d2c65b46b62238277ca71eb172978a9
Rith <harith.trmz@gmail.com>
2023-04-29 16:49:26 +0800

    css in js styled component

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,7 +1,21 @@
 import { useState } from "react";
-import styles from  "./ListGroup.module.css";
+import styled from "styled-components";
 
 // {items: [], heading: string}
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+interface ListItemProps {
+  active: boolean;
+}
+
+const ListItem = styled.li<ListItemProps>`
+  padding: 5px 0;
+  background: ${(props) => (props.active ? "blue" : "none")};
+`;
 interface Props {
   items: string[];
   heading: string;
@@ -26,21 +40,17 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
   // hook
   // selectedIndex = variable state
   // setSelectedIndex = function
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <>
       <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
-      <ul className={styles["list-group"]}>
+      <List>
         {items.map((item, index) => (
-          <li
+          <ListItem
+            active={index === selectedIndex}
             key={item}
-            className={
-              selectedIndex === index
-                ? "list-group-item active"
-                : "list-group-item"
-            }
             // just handleClick instead handleClick()
             // just pass reference
             onClick={() => {
@@ -49,9 +59,9 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
             }}
           >
             {item}
-          </li>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 }
