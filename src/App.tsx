1e1451d886ffbeeb913eed8a8ec338ec58b7da17
Rith <harith.trmz@gmail.com>
2023-04-29 17:37:35 +0800

    using css module

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
-import Button from "./components/Button";
+import Button from "./components/Button/Button";
 import { BsFillCalendarFill } from "react-icons/bs";
 
 function App() {
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,23 @@
+import styles from "./Button.module.css";
+
+interface Props {
+  children: string;
+  color?: "primary" | "secondary" | "danger";
+  onClick: () => void;
+}
+
+const Button = ({ children, color = "primary", onClick }: Props) => {
+  return (
+    <button
+      type="button"
+      // example to use two classes in one className
+      // recommended to naming the class to camelCase
+      className={[styles.btn, styles["btn-" + color]].join(" ")}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
