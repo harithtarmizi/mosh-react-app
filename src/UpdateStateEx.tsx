0b18cc2b5e4c524ef8e827e2ebd4eabac5c818cb
Rith <harith.trmz@gmail.com>
2023-04-29 23:44:18 +0800

    exercise building extandable text component

--- src/UpdateStateEx.tsx
+++ src/UpdateStateEx.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import ExpandableText from "./components/ExpandableText";
 
 function UpdateStateEx() {
   const [game, setGame] = useState({ id: 1, player: { name: "John" } });
@@ -29,6 +30,19 @@ function UpdateStateEx() {
   return (
     <div>
       <button onClick={handleClick}>Click me</button>
+      <ExpandableText>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quibusdam
+        praesentium est nisi, cum commodi error cumque quasi sequi soluta
+        officiis inventore consequuntur corporis suscipit incidunt quaerat
+        doloremque? Cum, eum! Odit molestias et in voluptate amet placeat
+        blanditiis laudantium voluptatum eligendi veniam cumque quae veritatis
+        ut eum deleniti libero sit magnam, voluptates neque ex obcaecati ducimus
+        perferendis? Totam quae nostrum cum iusto, sed, expedita et enim
+        similique est distinctio voluptates hic mollitia odio. Dolores,
+        laboriosam illo dicta sit quidem, ab veniam, debitis iusto laborum
+        suscipit ipsa maiores fuga cum deserunt commodi architecto? Totam enim
+        aliquam sequi aperiam eum quaerat quas!
+      </ExpandableText>
     </div>
   );
 }
