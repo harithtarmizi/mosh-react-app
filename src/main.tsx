6e175d4b1ecc6731b05d4cf1d417e8250e158172
Rith <harith.trmz@gmail.com>
2023-04-30 15:55:37 +0800

    building a form

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
 import App from "./App";
 import State from "./State";
 import ShoppingCart from "./ShoppingCart";
 import UpdateStateEx from "./UpdateStateEx";
+import FormPage from "./FormPage";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <State /> */}
     {/* <ShoppingCart /> */}
-    <UpdateStateEx />
+    {/* <UpdateStateEx /> */}
+    <FormPage />
   </React.StrictMode>
 );
