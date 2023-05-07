99a1c64885edf0e5b39213a7e78c0f89b1125494
Rith <harith.trmz@gmail.com>
2023-05-07 23:31:58 +0800

    accessing input fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,37 @@
+import { FormEvent, useRef } from "react";
+
 const Form = () => {
+  // we want to referencing a HTML input element
+  // why pull initial value null, because DOM will have after render
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: "", age: 0 };
+
+  const handleSubmit = (e: FormEvent) => {
+    // by default submit will send to server
+    e.preventDefault();
+    if (nameRef.current !== null) {
+      person.name = nameRef.current.value;
+    }
+    if (ageRef.current !== null) {
+      person.age = parseInt(ageRef.current.value);
+    }
+    console.log(person);
+  };
   return (
-    <form>
+    // remember, we dont put function inside onSubmit but just reference, hence it doesn't have parethesis
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={nameRef} id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={ageRef} id="age" type="number" className="form-control" />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
@@ -20,4 +40,4 @@ const Form = () => {
   );
 };
 
-export default Form
+export default Form;
