8906bd2860469f92d7cfe90d86906057700ac346
Rith <harith.trmz@gmail.com>
2023-05-08 22:58:49 +0800

    controlled components

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,21 +1,20 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useRef, useState } from "react";
 
 const Form = () => {
   // we want to referencing a HTML input element
   // why pull initial value null, because DOM will have after render
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: 0 };
+  // const nameRef = useRef<HTMLInputElement>(null);
+  // const ageRef = useRef<HTMLInputElement>(null);
+  // const person = { name: "", age: 0 };
+
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
 
   const handleSubmit = (e: FormEvent) => {
     // by default submit will send to server
     e.preventDefault();
-    if (nameRef.current !== null) {
-      person.name = nameRef.current.value;
-    }
-    if (ageRef.current !== null) {
-      person.age = parseInt(ageRef.current.value);
-    }
     console.log(person);
   };
   return (
@@ -25,13 +24,27 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          id="name"
+          type="text"
+          className="form-control"
+          value={person.name}
+          onChange={(e) => setPerson({ ...person, name: e.target.value })}
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          id="age"
+          type="number"
+          className="form-control"
+          value={person.age}
+          onChange={(e) =>
+            setPerson({ ...person, age: (e.target.value) })
+          }
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
