f37c491e5e3872ec0e454128026fddbb088a716b
Rith <harith.trmz@gmail.com>
2023-05-11 01:33:01 +0800

    managing forms with react hook form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { FormEvent, useRef, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 const Form = () => {
   // we want to referencing a HTML input element
@@ -7,19 +8,24 @@ const Form = () => {
   // const ageRef = useRef<HTMLInputElement>(null);
   // const person = { name: "", age: 0 };
 
+  const { register, handleSubmit } = useForm();
+
   const [person, setPerson] = useState({
     name: "",
     age: "",
   });
 
-  const handleSubmit = (e: FormEvent) => {
-    // by default submit will send to server
-    e.preventDefault();
-    console.log(person);
-  };
+  // const handleSubmit = (e: FormEvent) => {
+  //   // by default submit will send to server
+  //   e.preventDefault();
+  //   console.log(person);
+  // };
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
   return (
     // remember, we dont put function inside onSubmit but just reference, hence it doesn't have parethesis
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
@@ -28,8 +34,9 @@ const Form = () => {
           id="name"
           type="text"
           className="form-control"
-          value={person.name}
-          onChange={(e) => setPerson({ ...person, name: e.target.value })}
+          // value={person.name}
+          // onChange={(e) => setPerson({ ...person, name: e.target.value })}
+          {...register("name")}
         />
       </div>
       <div className="mb-3">
@@ -40,10 +47,9 @@ const Form = () => {
           id="age"
           type="number"
           className="form-control"
-          value={person.age}
-          onChange={(e) =>
-            setPerson({ ...person, age: (e.target.value) })
-          }
+          // value={person.age}
+          // onChange={(e) => setPerson({ ...person, age: e.target.value })}
+          {...register("age")}
         />
       </div>
       <button className="btn btn-primary" type="submit">
